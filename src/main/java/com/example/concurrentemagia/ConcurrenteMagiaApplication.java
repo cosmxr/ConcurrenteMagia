af3ab037cbac885e3bb28be9de378cd4e66c27c7
cosmxr <marcrues@myuax.com>
2024-10-27 18:55:59 +0100

    Initial commit

---
+++ src/main/java/com/example/concurrentemagia/ConcurrenteMagiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentemagia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrenteMagiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrenteMagiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concurrentemagia/ConcurrenteMagiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrentemagia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrenteMagiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
